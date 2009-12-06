b135b5f8d46498b800e22c10cd279c76089d9aca
tomasmajer@gmail.com <tomasmajer@gmail.com@f97d4978-d921-11de-b113-f7137c4feea6>
2009-12-06 19:43:36 +0000

    momentalne nie funkcna verzia

--- trunk/app/core/Tabulka.php
+++ trunk/app/core/Tabulka.php
@@ -26,6 +26,70 @@ class Tabulka
 		throw new TableKeyNotFoundException("Nenasla sa hodnota pre kombinaciu [$neterminal][$terminal]");
 	}
 	
+	public function getHTMLTable()
+	{
+		$tmpTable = array();
+		foreach ($this->data as $row)
+		{
+			$first = $row[0];
+			$second = $row[1];
+			$tmpTable[$first->getRepresentation()][$second->getRepresentation()] = $row[3];
+		}
+		echo "<pre>";
+		print_r($tmpTable);
+		echo "</pre>";
+		
+		$content = '<html><body>';
+		$content .= '<table BORDER>';
+		
+		$content .= '<thead><tr><td></td>';
+		$terminaly = AppConfig::get('terminaly');
+		foreach ($terminaly as $terminal)
+		{
+			$content .= '<td>' . htmlspecialchars($terminal) . '</td>';
+		}
+		$content .= '</tr></thead>';
+		$content .= '<tbody>';
+		
+		foreach ($tmpTable as $neterminal => $data)
+		{
+			$content .= '<tr>';
+			$content .= '<td>' . htmlspecialchars($neterminal) . '</td>';
+			foreach ($terminaly as $terminal)
+			{
+				$content .= '<td>';
+				$found = false;
+				foreach ($tmpTable as $kNeterminal => $kData)
+				{
+					if ($kNeterminal == $neterminal)
+					{
+						foreach ($kData as $term => $prechod)
+						{
+							if ($term == $terminal)
+							{
+								$found = $prechod;
+							}
+						}
+					}
+				}
+				if ($found) $content .= $found;
+				else $content .= '-';
+				$content .= '</td>';
+			}
+
+			$content .= '</tr>';
+		}
+		
+		$content .= '</tbody>';
+		$content .= '</table>';
+		
+		
+		
+		$content .= '</html>';
+		//print_r($this->data);
+		return $content;
+	}
+	
 	public function getLastPosition()
 	{
 		return $this->lastPosition;
